7ab293c6197709e10d88df57b3574a1af179b2fa
ajssal20 <ajssal20@htl-shkoder.com>
2025-04-12 14:41:23 +0200

    added page to create new articles

---
+++ src/routes/admin/new/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { enhance } from '$app/forms';
+</script>
+
+<h1>Create a new article</h1>
+
+<form method="POST" action="?/createArticle" enctype="multipart/form-data" use:enhance>
+    <label for="image">Image</label>
+	<input type="file" name="image" />
+
+	<label for="description">Description</label>
+	<textarea type="text" placeholder="Description" name="description"></textarea>
+
+	<label for="author">Author</label>
+	<input type="text" placeholder="Author" name="author" />
+
+	<button type="submit">Create article</button>
+</form>
