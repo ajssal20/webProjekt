ff1bb92fe7faf5c5daecf29c8a0c47458df20a87
ajssal20 <ajssal20@htl-shkoder.com>
2025-04-12 14:00:19 +0200

    created admin page

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { enhance } from '$app/forms';
+	import { slide } from 'svelte/transition';
+	let { data } = $props();
+</script>
+
+<div>
+	<h1>Articles</h1>
+
+	<a href="/admin/new">Create a new article</a>
+</div>
+
+{#each data.articles as article (article.id)}
+	<div transition:slide>
+		<p>{article.id} - {article.description} - {article.author} - {article.votes}</p>
+
+		<form action="?/deleteArticle" method="POST" use:enhance>
+			<input type="hidden" name="id" value={article.id} />
+			<button type="submit">Delete</button>
+		</form>
+	</div>
+{/each}
